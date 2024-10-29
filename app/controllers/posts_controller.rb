85ba4edd2acf2355dda0eb3a5c426f5122f471bb
momo-41 <minimizu41@gmail.com>
2024-10-29 20:10:52 +0900

    indexとnewのコントローラを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
-    def index
+    def index # index.html.erbファイルで@postを呼ぶことができる
+        @posts = Post.all # Post.allはデータベースから情報を全て取得している
+    end
+
+    def new # 新しい掲示板の投稿のフォーム用のコントローラー(new.html.erbファイルで使える)
+        @posts = Post.new # Postというtitleとcontentが含まれた情報が保存される予定
     end
 end
